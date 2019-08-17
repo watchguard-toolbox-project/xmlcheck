e3232cd0740d6c1d4673aa92df16348dd2233975
Werner Maier <werner@maiers.de>
2019-08-18 00:59:49 +0200

    suppress interface aliases in alias-list

--- lib/BOC/WatchGuardAlias.php
+++ lib/BOC/WatchGuardAlias.php
@@ -9,14 +9,29 @@ class WatchGuardAlias
     private $alias;
     private $referencedBy;
     private $refcount;
+    private $aliastype;
 
-    public function __construct(SimpleXMLElement $element) {
+    public function __construct(SimpleXMLElement $element)
+    {
         $this->alias = $element;
         $this->referencedBy = [];
         $this->refcount = 0;
+
+        $members = $this->alias->{'alias-member-list'}->{'alias-member'};
+        if (count($members) == 1) {
+            // only one member, check for interface...
+            $member = $members[0];
+            $type = $member->type;
+
+            if ($type == 1 && $member->interface->__toString() != "Any") {
+                $this->aliastype = "interface";
+            } else {
+                $this->aliastype = "other";
+            }
+        }
     }
 
-    public function getReferencedAliases() {
+        public function getReferencedAliases() {
 
         $retval = [];
 
@@ -41,15 +56,21 @@ class WatchGuardAlias
         $this->refcount++;
     }
 
-    public function textout($xmlfile) {
+    public function textout($xmlfile)
+    {
 
         global $options;
 
-        print $this->alias->name;
-        print $this->refcount == 0 ? " (unused)\n" : "\n";
+        if ($this->aliastype != "interface"
+            && !isset($options["verbose"])
+            && $this->alias->name->__toString() != "dvcp_nets"
+            && $this->alias->name->__toString() != "Any"
+        ) {
+            print $this->alias->name;
+            print $this->refcount == 0 ? " (unused)\n" : "\n";
+        }
 
         if (isset($options["verbose"])) {
-
 
             $memberlist = $this->alias->{'alias-member-list'};
 
@@ -59,15 +80,21 @@ class WatchGuardAlias
                 $member = $memberlist->{'alias-member'}[$nr];
                 $type = $member->type;
 
+                // prepare printf statement based on interface/address/alias/etc.
                 switch ($type) {
                     case 1:
-                        $typestring = "address";
-                        $value = $memberlist->{'alias-member'}[$nr]->address->__toString();
-                        $content = $xmlfile->resolveAliasAddress($value);
+                        if ($member->interface->__toString() != "Any") {
+                            $typestring = "interface";
+                            $value = $member->interface->__toString();
+                        } else {
+                            $typestring = "address";
+                            $value = $member->address->__toString();
+                            $content = $xmlfile->resolveAliasAddress($value);
+                        }
                         break;
                     case 2:
                         $typestring = "alias";
-                        $value = $memberlist->{'alias-member'}[$nr]->{'alias-name'};
+                        $value = $member->{'alias-name'};
                         break;
                     default:
                         $value = "unknown type";
