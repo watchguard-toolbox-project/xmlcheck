190356bf0671e8ab5639ce1d28dfd9277b629169
Werner Maier <werner@maiers.de>
2019-08-17 22:22:40 +0200

    new action alias started

--- xmlcheck.php
+++ xmlcheck.php
@@ -8,11 +8,11 @@ require_once(dirname(__FILE__) . '/vendor/autoload.php');
 require_once("lib/functions.php");
 require_once("lib/options.php");
 
-// globale Variablen us options:
+// global variables from options.php:
 // $options
 // $xmlfile
 
-$policyxml = new BOC\WatchGuardXMLFile($xmlfile);
+$policyxml = new WatchGuardXMLFile($xmlfile);
 
 if (isset($options["listaliases"])) {
     $policyxml->listAllAliases();
@@ -20,4 +20,9 @@ if (isset($options["listaliases"])) {
 
 if (isset($options["listpolicies"])) {
     $policyxml->listAllPolicies();
+}
+
+if (isset($options["alias"]) || isset($options["a"])) {
+    $printalias = isset($options["alias"]) ? $options["alias"] : $options["a"];
+    $policyxml->printAlias($printalias);
 }
