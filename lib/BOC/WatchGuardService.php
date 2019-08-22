8d2b14c2689c27f02bcc17ad9bc41fe906dbd42f
Werner Maier <wm@boc.de>
2019-08-22 23:01:01 +0200

    phpdoc comments

--- lib/BOC/WatchGuardService.php
+++ lib/BOC/WatchGuardService.php
@@ -1,16 +1,33 @@
 <?php
-
+/**
+ * Classfile for WatchGuardService Object.
+ *
+ * @author       Werner Maier <wm@boc.de>
+ * @copyright    (C) 2019 BOC IT-Security GmbH
+ */
 namespace BOC;
 
 use SimpleXMLElement;
 
+/**
+ * Class WatchGuardService
+ * @package BOC
+ */
 class WatchGuardService extends WatchGuardObject
 {
 
+    /**
+     * WatchGuardService constructor.
+     * @param SimpleXMLElement $element
+     */
     public function __construct(SimpleXMLElement $element) {
         parent::__construct($element);
     }
 
+    /**
+     * Returns the property element of $this
+     * @return string
+     */
     public function getProperty() {
         $object = $this->obj;
         return $object->property->__toString();
--- lib/BOC/WatchGuardXMLFile.php
+++ lib/BOC/WatchGuardXMLFile.php
@@ -1,19 +1,58 @@
 <?php
+/**
+ * this contains the base class file
+ *
+ * here go methods for loading xmlfile and initaliyng all objects,
+ * as well as crosschecking and printing loops.
+ *
+ * @author Werner Maier (wm@boc.de)
+ * @copyright 2019 BOC IT-Security GmbH, www.boc.de
+ */
 
+/**
+ * dummy
+ */
 namespace BOC;
 
 // use BOC\WatchGuardAlias;
 use SimpleXMLElement;
 
+/**
+ * Class WatchGuardXMLFile
+ * @package BOC
+ */
 class WatchGuardXMLFile
 {
+    /**
+     * base object containing the loaded xml structure and all methods
+     * @var SimpleXMLElement
+     */
     private $xmlfile;       // complete xml structure as SimpleXML object
 
     // arrays for all aliases, policies and services:
+    /**
+     * array for all aliases from this xmlfile
+     * @var array
+     */
     private $allAliases;
+    /**
+     * array for all policies from this xmlfile
+     * @var array
+     */
     private $allPolicies;
+    /**
+     * array for all services from this xmlfile
+     * @var array
+     */
     private $allServices;
 
+    /**
+     * WatchGuardXMLFile constructor.
+     *
+     * loads the xmlfile, initializes all aliases, policies and services.
+     *
+     * @param $xmlfilename
+     */
     public function __construct($xmlfilename) {
         $this->xmlfile = simplexml_load_file($xmlfilename);
         $this->allAliases  = [];
@@ -28,7 +67,7 @@ class WatchGuardXMLFile
     }
 
     /**
-     * Helpers
+     * reads all aliases from xml and sets the pointer into allAliases array
      **/
     private function getAllAliases() {
 
@@ -38,6 +77,12 @@ class WatchGuardXMLFile
 
     }
 
+    /**
+     * reads all policies from xml and sets the pointer into allPolicies array
+     *
+     * also initializes the alias references; if an alias is referenced by this policy,
+     * it is stored at the alias object.
+     */
     private function getAllPolicies() {
 
         foreach ($this->xmlfile->{'policy-list'}->{'policy'} as $policy) {
@@ -48,6 +93,9 @@ class WatchGuardXMLFile
 
     }
 
+    /**
+     * reads all services from xml and sets the pointer into allServices array
+     */
     private function getAllServices() {
 
         foreach ($this->xmlfile->{'service-list'}->children() as $service) {
@@ -56,7 +104,14 @@ class WatchGuardXMLFile
 
     }
 
-    private function getXMLObject(SimpleXMLElement $obj,$searchname) {
+    /**
+     * searchs an xml object by name and returns the found simpleXMLElement
+     *
+     * @param SimpleXMLElement $obj
+     * @param $searchname
+     * @return SimpleXMLElement|null
+     */
+    private function getXMLObject(SimpleXMLElement $obj, $searchname) {
         $retval = null;
 
         foreach ($obj->children() as $child) {
@@ -68,6 +123,15 @@ class WatchGuardXMLFile
         return $retval;
     }
 
+    /**
+     * resolves aliases for printing out the details
+     *
+     * currently supports addresses and networks (addr/mask).
+     * and partly things like interfaces.
+     *
+     * @param $searchstring
+     * @return string
+     */
     public function resolveAliasAddress($searchstring)
     {
 
@@ -89,6 +153,7 @@ class WatchGuardXMLFile
                     case 3:
                         $retval = $member->{'start-ip-addr'}->__toString() . "-" . $member->{'end-ip-addr'}->__toString();
                         break;
+                        // TODO: other alias types like alias, groups, etc.
                     case 8:
                         $retval = $member->{'domain'}->__toString();
                         break;
@@ -101,6 +166,13 @@ class WatchGuardXMLFile
         return $retval;
     }
 
+    /**
+     * finds alias references and stores the reference at the object.
+     *
+     * or at least tries to. could be more sophisticated, but this is
+     * a quick start with hitting feels like about 98%
+     *
+     */
     public function findAliasReferences() {
 
         foreach ($this->allAliases as $aliasName => $alias) {
@@ -148,6 +220,9 @@ class WatchGuardXMLFile
 
     }
 
+    /**
+     * stores policy references to alias objects
+     */
     public function findAliasRefByPolicy() {
 
         foreach ($this->allPolicies as $policyName => $policy) {
@@ -164,6 +239,9 @@ class WatchGuardXMLFile
 
     }
 
+    /**
+     * stores policy references to services
+     */
     public function findServiceRefByPolicy() {
 
         foreach ($this->allPolicies as $policyName => $policy) {
@@ -181,6 +259,10 @@ class WatchGuardXMLFile
     /**
      * Actions
      **/
+
+    /**
+     * lists (all) aliases in this xmlfile
+     */
     public function listAllAliases() {
         foreach ($this->allAliases as $aliasName => $alias) {
             // ignore referenced aliases ending .1.from or .1.to or .from.[1234]
@@ -192,18 +274,28 @@ class WatchGuardXMLFile
         }
     }
 
+    /**
+     * lists (all) policies in this xmlfile
+     */
     public function listAllPolicies() {
         foreach ($this->allPolicies as $policyName => $policy) {
             $policy->textout($this);
         }
     }
 
+    /**
+     * lists (all) services in this xmlfile
+     */
     public function listAllServices() {
         foreach ($this->allServices as $serviceName => $service) {
             $service->textout($this);
         }
     }
 
+    /**
+     * prints one single alias
+     * @param $aliasname
+     */
     public function printAlias($aliasname) {
         if (isset($this->allAliases[$aliasname])) {
             $this->allAliases[$aliasname]->textout($this);
