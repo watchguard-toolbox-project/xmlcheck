b0879a71575ffd7b693ead4b769e00415c32b329
Werner Maier <wm@boc.de>
2019-08-22 23:24:25 +0200

    phpdoc comments

--- lib/BOC/WatchGuardPolicy.php
+++ lib/BOC/WatchGuardPolicy.php
@@ -1,21 +1,49 @@
 <?php
+/**
+ * Classfile for WatchGuardPolicy Object.
+ *
+ * @author       Werner Maier <wm@boc.de>
+ * @copyright    (C) 2019 BOC IT-Security GmbH
+ */
 
 namespace BOC;
 
 use SimpleXMLElement;
 
+/**
+ * Class WatchGuardPolicy
+ * @package BOC
+ */
 class WatchGuardPolicy extends WatchGuardObject
 {
 
+    /**
+     * stores all aliases from to: part
+     * @var array
+     */
     private $aliasesTo;
+    /**
+     * stores all aliases from from: part
+     * @var array
+     */
     private $aliasesFrom;
 
+    /**
+     * WatchGuardPolicy constructor.
+     * @param SimpleXMLElement $element
+     */
     public function __construct(SimpleXMLElement $element) {
         parent::__construct($element);
         $this->aliasesTo   = [];
         $this->aliasesFrom = [];
     }
 
+    /**
+     * returns an array of alias names, that are referenced in an simppleXMLElement list
+     * like from: or to:
+     * @param $list simpleXMLElement
+     * @return array
+     */
     private function getReferencedAliasesFromAliasList($list) {
 
         $retval = [];
@@ -28,6 +56,10 @@ class WatchGuardPolicy extends WatchGuardObject
         return $retval;
     }
 
+    /**
+     * find referenced aliases in policy: look at from:, look at to:
+     * @return array
+     */
     public function getReferencedAliases() {
 
         $retval = [];
@@ -43,18 +75,34 @@ class WatchGuardPolicy extends WatchGuardObject
         return $retval;
     }
 
+    /**
+     * stores the alias into the aliasesTo array.
+     * @param $aliasarray
+     */
     public function storeAliasesTo ($aliasarray) {
         $this->aliasesTo = array_merge($this->aliasesTo, $aliasarray);
     }
 
+    /**
+     * stores the alias into the aliasesFrom array.
+     * @param $aliasarray
+     */
     public function storeAliasesFrom ($aliasarray) {
         $this->aliasesFrom = array_merge($this->aliasesTo, $aliasarray);
     }
 
+    /**
+     * returns the name of the service of this policy
+     * @return string
+     */
     public function getService() {
         return $this->obj->service->__toString();
     }
 
+    /**
+     * detailed printout of policy information
+     * @param WatchGuardXMLFile $xmlfile
+     */
     protected function verbosetextout($xmlfile)
     {
         global $options;
@@ -72,6 +120,10 @@ class WatchGuardPolicy extends WatchGuardObject
         print "\n";
     }
 
+    /**
+     * print this policy
+     * @param WatchGuardXMLFile $xmlfile
+     */
     public function textout($xmlfile) {
         global $options;
 
